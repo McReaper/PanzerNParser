78565ce80baaffd08f2642b135278586a531592a
emilie <emilie.tondeux@etu.univ-grenoble-alpes.fr>
2020-04-14 09:45:08 +0200

    creation constructeur

--- workspace/cyance/src/automate/Entity.java
+++ workspace/cyance/src/automate/Entity.java
@@ -1,5 +1,24 @@
 package automate;
 
-public class Entity {
+public abstract class Entity {
+	int m_x;
+	int m_y;
+	int m_width;
+	int m_height;
+	boolean m_exist;
+	boolean hit;
+	
+	public Entity() {
+		m_x = 0;
+		m_y = 0;
+		m_width = 0;
+		m_height = 0;
+		m_exist = false;
+	}
 
+	public void hit() {
+		if (m_exist) {
+			hit = true;
+		}
+	}
 }

603fa57669fab1417788a6e01d3bdd37bf0272ae
Mathis MUTEL <mutelm@etu.univ-grenoble-alpes.fr>
2020-04-18 10:53:57 +0200

    création Transition et Behaviour

---
+++ workspace/groupe2.info3.game/panzernparser/info3/game/myAutotmaton/Behaviour.java
@@ -0,0 +1,13 @@
+package info3.game.myAutotmaton;
+
+import java.util.List;
+
+public class Behaviour {
+
+	List<Transition> m_transitions;
+
+	public Behaviour(List<Transition> transition) {
+		m_transitions = transition;
+	}
+
+}

860eefc41b3bbb350f6bf007580b63607799e60b
emilie <emilie.tondeux@etu.univ-grenoble-alpes.fr>
2020-04-18 14:57:07 +0200

    reorganisation du la classe AutomatonMain pour plus de clarté

--- workspace/groupe2.info3.game/panzernparser/info3/game/myAutotmaton/AutomatonMain.java
+++ workspace/groupe2.info3.game/panzernparser/info3/game/myAutotmaton/AutomatonMain.java
@@ -7,24 +7,41 @@ import info3.game.model.Entity;
 
 
 public class AutomatonMain {
+	
 	public static void main(String[] args) {
 		Entity e = new Enemy();
 		List<Mode> modes = new LinkedList<Mode>();
 		List<Transition> transitions = new LinkedList<Transition>();
 		List<FunCall> funCalls = new LinkedList<FunCall>();
-		Condition c1 = new True();
-		FunCall fc = new Egg(100);
-		funCalls.add(fc);
-		Action action = new Action(funCalls);
 		State state = new State("init");
-		Transition t1 = new Transition(c1, action, state);
-		transitions.add(t1);
+		
+		initFunCalls(funCalls);
+		initTransition(transitions, state, funCalls);
+		
 		Behaviour behaviour = new Behaviour(transitions);
-		Mode mode1 = new Mode(behaviour, state);
-		modes.add(mode1);
+		
+		initMode(modes, behaviour, state);
+		
 		Automaton aut = new Automaton("auto", (LinkedList<Mode>) modes, state);
 		while(true) {
 			aut.step(e);
 		}
+	}
+	
+	private static void initFunCalls(List<FunCall> funCalls) {
+		FunCall fc = new Egg(100);
+		funCalls.add(fc);
+	}
+	
+	private static void initTransition(List<Transition> transitions, State init, List<FunCall> funCalls) {
+		Condition c1 = new True();
+		Action action = new Action(funCalls);
+		Transition t1 = new Transition(c1, action, init);
+		transitions.add(t1);
+	}
+	
+	private static void initMode(List<Mode> modes, Behaviour behaviour, State init) {
+		Mode mode1 = new Mode(behaviour, init);
+		modes.add(mode1);
 	}
 }

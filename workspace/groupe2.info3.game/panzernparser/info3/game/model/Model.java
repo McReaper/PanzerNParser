05ab2266dcf6d5553110c0eee41302561117d9b9
Mathis MUTEL <mutelm@etu.univ-grenoble-alpes.fr>
2020-04-20 17:05:34 +0200

    ajout lecture fichiers .gal dans le constructeur de Model

--- workspace/groupe2.info3.game/panzernparser/info3/game/model/Model.java
+++ workspace/groupe2.info3.game/panzernparser/info3/game/model/Model.java
@@ -1,7 +1,13 @@
 package info3.game.model;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
+import info3.game.automata.ast.AST;
+import info3.game.automata.parser.AutomataParser;
+import info3.game.automaton.*;
 import info3.game.model.entities.Entity;
 
 public class Model {
@@ -9,14 +15,30 @@ public class Model {
 	// Controller m_controller; //pour envoyer des information utiles.
 	Grid m_grid;
 	LinkedList<Entity> m_entities;
-	// TODO : Ajouter la liste des Automates
+	List<Automaton> m_automatons;
 
 	public Model() {
+		// Génère la liste des automates
+		try {
+			m_automatons = new ArrayList<Automaton>();
+			File repertoire = new File("../../automate.gal");
+			String[] fils = repertoire.list();
+			for (String curFil : fils) {
+				AST myAST = AutomataParser.from_file(curFil);
+				m_automatons.addAll((List<Automaton>) myAST.accept(new BotBuilder()));
+      } 
+			/* si un seul fichier .gal commentez début try et décomenter fin*/
+//			AST myAST = AutomataParser.from_file("../../automate.gal/*");
+//			m_automatons = (List<Automaton>) myAST.accept(new BotBuilder());
+		} catch (Exception e) {
+			System.out.println("fichier non trouvé pour la création des automates");
+			e.printStackTrace();
+		}
 		// Génère la grille du jeu qui va créer a son tour toutes les entités et mettre
 		// la liste des entités à jour. La grille doit connaitre ses patterns lors de sa
 		// création, le model doit donc lui donner.
 		m_entities = new LinkedList<Entity>();
-		
+
 		// Version de test ci-dessous :
 		m_grid = new Grid(this);
 	}
@@ -27,13 +49,13 @@ public class Model {
 			entity.step(elapsed);
 		}
 	}
-	
+
 	public Grid getGrid() {
 		return m_grid;
 	}
-	
+
 	public LinkedList<Entity> getEntities() {
 		return m_entities;
 	}
-	
+
 }

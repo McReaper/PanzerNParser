b029e6525b077a66e803bab8a2f391c963e27787
emilie <emilie.tondeux@etu.univ-grenoble-alpes.fr>
2020-04-20 16:02:19 +0200

    implem de la classe avatar

--- workspace/groupe2.info3.game/panzernparser/info3/game/view/Avatar.java
+++ workspace/groupe2.info3.game/panzernparser/info3/game/view/Avatar.java
@@ -11,13 +11,30 @@ import info3.game.model.entities.Entity;
 public class Avatar {
 	Sprite m_sprite;
 	Entity m_entity;
+	int m_idImageSprite;
 
 	public Avatar(Entity entity) {
 		m_entity = entity;
+		m_idImageSprite = 1;
+		m_sprite = new  Sprite("../sprites/Minerai.png");
+	}
+	
+	public boolean nextImage() {
+		if (m_idImageSprite<= m_sprite.nbSprites()) {
+			m_idImageSprite ++;
+			return true;
+		}else {
+			m_idImageSprite = 1;
+			return true;
+		}
 	}
 	
 	public void paint(Graphics g) {
-		
+		int x = m_entity.getX();
+		int y = m_entity.getY();
+		int scale = 1;
+		g.drawImage(m_sprite.getSprite(m_idImageSprite), x, y, scale, scale);
+		nextImage();
 	}
 
 }

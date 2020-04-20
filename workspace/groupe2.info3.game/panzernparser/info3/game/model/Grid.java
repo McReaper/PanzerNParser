004cea4ad97d4e7dbe4dfeaa9543899173225111
baudeurb <baudeurbertrand@yahoo.fr>
2020-04-20 15:47:45 +0200

    Grid squelette et MyEntities

--- workspace/groupe2.info3.game/panzernparser/info3/game/model/Grid.java
+++ workspace/groupe2.info3.game/panzernparser/info3/game/model/Grid.java
@@ -1,6 +1,11 @@
 package info3.game.model;
 
+import java.io.FileInputStream;
+import java.util.List;
+
 import info3.game.model.entities.Enemy;
+import info3.game.model.entities.Entity;
+import info3.game.model.entities.MyEntities;
 
 /**
  * Créer la grille de jeu avec les entités correspondantes en fonction d'un ou
@@ -10,10 +15,11 @@ public class Grid {
 	int m_nbCellsX;
 	int m_nbCellsY;
 	Model m_model;
+	List<Pattern> m_patterns;
 
 	public Grid(Model model) {
 		// Constructeur (phase de tests) :
-		m_nbCellsX = 3;
+		m_nbCellsX = 6;
 		m_nbCellsY = 3;
 		m_model = model;
 		m_model.m_entities.add(new Enemy(1, 1, 1, 1));
@@ -25,6 +31,35 @@ public class Grid {
 
 	public int getNbCellsY() {
 		return m_nbCellsY;
+	}
+	
+	public void generate() {
+		
+	}
+	
+	public void sendToModel() {
+		
+	}
+
+	private class Pattern {
+		
+		private class EntityShade {
+			MyEntities m_type;
+			int m_ex,m_ey;
+		}
+		
+		private final int SIZE = 3;
+		int m_px,m_py;
+		List<EntityShade> m_entities;
+		
+		List<Entity> getEntities() {
+			return null;
+		}
+		
+		private void parse(FileInputStream is) {
+			
+		}
+		
 	}
 
 }
---
+++ workspace/groupe2.info3.game/panzernparser/info3/game/model/entities/MyEntities.java
@@ -0,0 +1,5 @@
+package info3.game.model.entities;
+
+public enum MyEntities {
+	TANK,DRONE,WALL,GROUND,ENEMY,DROPPABLE,SHOT,VEIN; //TODO
+}

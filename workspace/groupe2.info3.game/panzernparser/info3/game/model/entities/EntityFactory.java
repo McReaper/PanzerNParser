39231724adabaacad4085a8e622bc9897669ae7b
Sami Elhadji <sami.elhadji-tchiambou@etu.univ-grenoble-alpes.fr>
2020-04-21 12:08:13 +0200

    merge branche principale sur secondaire dans grid

--- workspace/groupe2.info3.game/panzernparser/info3/game/model/entities/EntityFactory.java
+++ workspace/groupe2.info3.game/panzernparser/info3/game/model/entities/EntityFactory.java
@@ -7,41 +7,41 @@ public class EntityFactory {
 		WALL, GROUND, ENEMY, DROPPABLE, SHOT, VEIN; // TODO
 	}
 
-	public Entity newTank() {
+	public static Entity newTank() {
 		return null;
 	}
 
-	public Entity newDrone() {
+	public static Entity newDrone() {
 		return null;
 	}
 
-	public Entity newMarker() {
+	public static Entity newMarker() {
 		return null;
 	}
 
-	public Entity newShot(int x, int y) {
+	public static Entity newShot(int x, int y) {
 		return new Shot(x, y, Shot.SHOT_WIDTH, Shot.SHOT_HEIGHT, 10, 10);
 		/* par defaut health et time_to_travel = 10 mais a voir où gerer ça */
 	}
 
-	public Entity newEnemy(int x, int y) {
+	public static Entity newEnemy(int x, int y) {
 		return new Enemy(x, y, Enemy.ENEMY_WIDTH, Enemy.ENEMY_HEIGHT);
 	}
 
-	public Entity newDroppable(int x, int y) {
+	public static Entity newDroppable(int x, int y) {
 		return new Droppable(x, y, Droppable.DROPPABLE_WIDTH, Droppable.DROPPABLE_HEIGHT, 1, MaterialType.ELECTRONIC);
 		// 1 en quantité mais à discuter
 	}
 
-	public Entity newVein(int x, int y) {
+	public static Entity newVein(int x, int y) {
 		return new Vein(x, y, Ground.GROUND_WIDTH, Ground.GROUND_HEIGHT);
 	}
 
-	public Entity newGround(int x, int y) {
+	public static Entity newGround(int x, int y) {
 		return new Ground(x, y, Ground.GROUND_WIDTH, Ground.GROUND_HEIGHT);
 	}
 
-	public Entity newEntity(MyEntities entity, int x, int y) {
+	public static Entity newEntity(MyEntities entity, int x, int y) {
 		Entity res;
 		switch (entity) {
 			case WALL:
@@ -73,7 +73,7 @@ public class EntityFactory {
 		return res;
 	}
 	
-	public String name(Entity entity) {
+	public static String name(Entity entity) {
 		System.out.println("test de entityFactory");
 		System.out.println(entity.getClass().getName());
 		return  entity.getClass().getName();

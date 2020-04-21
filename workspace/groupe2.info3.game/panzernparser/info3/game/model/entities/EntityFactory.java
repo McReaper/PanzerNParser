438682c860f5291982dfa9270a99e1d768937e6a
baudeurb <baudeurbertrand@yahoo.fr>
2020-04-21 09:05:33 +0200

    Création du squelette de entityFactory

---
+++ workspace/groupe2.info3.game/panzernparser/info3/game/model/entities/EntityFactory.java
@@ -0,0 +1,40 @@
+package info3.game.model.entities;
+
+public class EntityFactory {
+	public enum MyEntities {
+		TANK,DRONE,WALL,GROUND,ENEMY,DROPPABLE,SHOT,VEIN; //TODO
+	}
+
+	public Entity newTank() {
+		return null;
+	}
+	
+	public Entity newDrone() {
+		return null;
+	}
+	
+	public Entity newMarker() {
+		return null;
+	}
+	
+	public Entity newShot() {
+		return null;
+	}
+	
+	public Entity newEnemy() {
+		return null;
+	}
+	
+	public Entity newDroppable() {
+		return null;
+	}
+	
+	public Entity newVein() {
+		return null;
+	}
+	
+	public Entity newEntity(MyEntities entity) {
+		return null;
+	}
+	
+}
--- workspace/groupe2.info3.game/panzernparser/info3/game/model/entities/MyEntities.java
+++
@@ -1,5 +0,0 @@
-package info3.game.model.entities;
-
-public enum MyEntities {
-	TANK,DRONE,WALL,GROUND,ENEMY,DROPPABLE,SHOT,VEIN; //TODO
-}

486b308de98663aeee81c7b38084dfaf588d34d0
baudeurb <baudeurbertrand@yahoo.fr>
2020-04-25 17:43:00 +0200

    ViewPort Fonctionnel

--- workspace/groupe2.info3.game/panzernparser/info3/game/view/ViewPort.java
+++ workspace/groupe2.info3.game/panzernparser/info3/game/view/ViewPort.java
@@ -17,17 +17,17 @@ public class ViewPort {
 	Model m_model;
 	Entity m_player;
 	View m_view;
-	int m_width;
-	int m_height;
-	int m_nbCellsX;
-	int m_nbCellsY;
-	int case_width;
-	int case_height;
-	int m_x;
-	int m_y;
-	int m_offsetX;
-	int m_offsetY;
 	Grid m_grid;
+	int m_width; // Taille du canvas en pixel
+	int m_height;
+	int m_nbCellsX; //Nombre de cellule du view port
+	int m_nbCellsY;
+	int m_case_width; // Taille d'une case en pixel
+	int m_case_height;
+	int m_x; //Position du viewPort dans la grid /!\ Marge de 2 autour de ce que l'on voit
+	int m_y;
+	int m_offsetX; // Décalage propre au animations du move du player
+	int m_offsetY;
 
 	public ViewPort(Model model, Entity player, View view) {
 		m_model = model;
@@ -44,19 +44,19 @@ public class ViewPort {
 		m_nbCellsX = RANGE_VIEW * 2; // pour les deux coté du tank
 		m_nbCellsX += 1; // pour le tank
 		m_nbCellsX *= m_player.getWidth(); // pour que ça dépende de la taille de l'entity
-		case_width = m_width / m_nbCellsX;
-		case_height = case_width; // case carré
+		m_case_width = m_width / m_nbCellsX;
+		m_case_height = m_case_width; // case carré
 		m_nbCellsX += 4; // pour la marge
 		m_nbCellsY = m_nbCellsX;
-		// m_nbCellsY = m_height / case_height; // TODO géré si view porte pas carré
-		// m_height%case_height / 2 pour le decalage si pas le bon nombre de case
+		// m_nbCellsY = m_height / m_case_height; // TODO géré si view porte pas carré
+		// m_height%m_case_height / 2 pour le decalage si pas le bon nombre de case
 	}
 
 	private void positionViewPort() {
 		System.out.println("player (" + m_player.getX() + ";" + m_player.getY() + ")");
 		m_x = m_player.getX();
 		m_x -= RANGE_VIEW * m_player.getWidth();
-		m_x -= 2; //pour la marge
+		m_x -= 2; // pour la marge
 		m_x = m_grid.realX(m_x);
 		m_y = m_player.getY();
 		m_y -= RANGE_VIEW * m_player.getWidth(); // c'est normal c'est pour les entité non carré
@@ -75,12 +75,12 @@ public class ViewPort {
 				case NORTH:
 				case NORTHEAST:
 				case NORTHWEST:
-					m_offsetY -= (case_height * progress) - case_height;
+					m_offsetY -= (m_case_height * progress) - m_case_height;
 					break;
 				case SOUTH:
 				case SOUTHEAST:
 				case SOUTHWEST:
-					m_offsetY += (case_height * progress) - case_height;
+					m_offsetY += (m_case_height * progress) - m_case_height;
 					break;
 				default:
 					break;
@@ -89,12 +89,12 @@ public class ViewPort {
 				case EAST:
 				case NORTHEAST:
 				case SOUTHEAST:
-					m_offsetX += (case_width * progress) - case_width;
+					m_offsetX += (m_case_width * progress) - m_case_width;
 					break;
 				case WEST:
 				case NORTHWEST:
 				case SOUTHWEST:
-					m_offsetX -= (case_width * progress) - case_width;
+					m_offsetX -= (m_case_width * progress) - m_case_width;
 					break;
 				default:
 					break;
@@ -111,11 +111,14 @@ public class ViewPort {
 		offset(); // décalage due au mouve du tank
 		Entity e;
 		int x, y, w, h;
+		
+		//Décalage du aux dimensions de la fenêtre (centrage en y)
+		int offsetWindow = (m_nbCellsY * m_case_height - (m_height + 4 * m_case_height)) / 2;
 		g.setColor(Color.BLACK);
-		for (int i = -m_offsetX; i < m_nbCellsX * case_width; i += case_width)
-			g.drawLine(i, 0, i, case_height * m_nbCellsY);
-		for (int j = -m_offsetY; j < m_nbCellsY * case_height; j += case_height)
-			g.drawLine(0, j, case_width * m_nbCellsX, j);
+		for (int i = -m_offsetX; i < m_nbCellsX * m_case_width; i += m_case_width)
+			g.drawLine(i, 0, i, m_case_height * m_nbCellsY - offsetWindow);
+		for (int j = -m_offsetY; j < m_nbCellsY * m_case_height; j += m_case_height)
+			g.drawLine(0, j - offsetWindow, m_case_width * m_nbCellsX, j - offsetWindow);
 		for (Avatar avatar : lsAvatars) {
 			e = avatar.m_entity;
 			x = e.getX();
@@ -131,28 +134,33 @@ public class ViewPort {
 				x -= 2;
 				y -= 2;
 				// position en px de la case
-				x *= case_width;
-				y *= case_height;
+				x *= m_case_width;
+				y *= m_case_height;
 				// position case en px avec décalage
 				x -= m_offsetX;
-				y -= m_offsetY;
+				y -= m_offsetY + offsetWindow;
 				switch (intView) {
+					//Téléportation pour rentrer dans le viewPort
 					case PAINT_MOVE_X:
-						x += m_grid.getNbCellsX() * case_width;
+						x += m_grid.getNbCellsX() * m_case_width;
 						break;
 					case PAINT_MOVE_Y:
-						y += m_grid.getNbCellsY() * case_height;
+						y += m_grid.getNbCellsY() * m_case_height;
 						break;
 					case PAINT_MOVE_XY:
-						x += m_grid.getNbCellsX() * case_width;
-						y += m_grid.getNbCellsY() * case_height;
+						x += m_grid.getNbCellsX() * m_case_width;
+						y += m_grid.getNbCellsY() * m_case_height;
 						break;
 				}
-				avatar.paint(g, x, y, case_width, case_height);
+				avatar.paint(g, x, y, m_case_width, m_case_height);
 
 			}
 
 		}
+		//Pour empêcher de regarder plus loin grâce à la redimension de la fenêtre
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, m_width, (m_height - m_width) / 2);
+		g.fillRect(0, (m_height + m_width) / 2, m_width, (m_height - m_width) / 2);
 
 	}
 
@@ -162,17 +170,7 @@ public class ViewPort {
 	private static final int PAINT_MOVE_Y = 2;
 	private static final int PAINT_MOVE_XY = 3;
 
-	private int inView(int x, int y, int w, int h) { // TODO géré les cas ou les viewport est sur "plusieurs map"
-																										// utiliser + et -
-		// if ((x + w) > (m_x - 2) && x < (m_x + m_nbCellsX + 2) && (y + h) > (m_y - 2)
-		// && y < (m_y + m_nbCellsY + 2)) {
-		/// return true;
-		// }
-		/*
-		 * TODO : Il reste un problème pour quand on est sur les deux cases du bord de
-		 * map (haut et gauche)
-		 * 
-		 */
+	private int inView(int x, int y, int w, int h) {
 		int xL = m_x;
 		int xR = m_grid.realX(m_x + m_nbCellsX);
 		int yU = m_y;

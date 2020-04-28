d98fd9554eb45e910e60b4bdab89b88134d14e75
Sami Elhadji <sami.elhadji-tchiambou@etu.univ-grenoble-alpes.fr>
2020-04-28 16:44:58 +0200

    a

---
+++ workspace/groupe2.info3.game/panzernparser/info3/game/view/MarkerAvatar.java
@@ -0,0 +1,33 @@
+package info3.game.view;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import info3.game.automaton.MyDirection;
+import info3.game.automaton.action.LsAction;
+import info3.game.model.entities.Entity;
+
+public class MarkerAvatar extends Avatar{
+
+	public MarkerAvatar(Entity entity, Animation animation) {
+		super(entity, animation);
+	}
+
+	@Override
+	public void paint(Graphics g, int xcase, int ycase, int case_width, int case_height) {
+		MyDirection e_actionDir = null;
+		LsAction e_currAction = LsAction.Wait;
+		double progress = m_entity.getActionProgress();
+
+		int width = m_entity.getWidth() * case_width;
+		int height = m_entity.getHeight() * case_height;
+		int x = xcase;																						// deux seul changement
+		int y = ycase;
+
+		// Pour réaliser un affichage progressif dans le cas d'un move.
+
+		Image sprite = m_animation.getImage(progress, e_currAction, e_actionDir);
+
+		g.drawImage(sprite, x, y, width, height, null);
+	}
+}

17619ba44b2785b4295c080c8cca0d1603813fb5
baudeurb <baudeurbertrand@yahoo.fr>
2020-05-02 19:29:06 +0200

    Implémentation boutons en cours

--- workspace/groupe2.info3.game/panzernparser/info3/game/view/HUD.java
+++ workspace/groupe2.info3.game/panzernparser/info3/game/view/HUD.java
@@ -1,6 +1,8 @@
 package info3.game.view;
 
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 import java.util.Hashtable;
 import java.util.LinkedList;
 
@@ -8,21 +10,24 @@ import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.plaf.basic.*;
 
+import com.sun.corba.se.spi.legacy.connection.GetEndPointInfoAgainException;
+
+import javax.swing.ToolTipManager;
+
 import info3.game.model.*;
 import info3.game.model.entities.*;
 import info3.game.model.entities.EntityFactory.MyEntities;
 
 public class HUD {
-	
+
 	public static final int MINIMAL_WIDTH = 2 * 120;
 	public static final int MINIMAL_HEIGHT = 600;
-	
 
 	View m_view;
 
 	JPanel m_West;
 	JPanel m_East;
-	
+
 	JPanel m_ammoPanel;
 	JPanel m_viewModePanel;
 	JLabel m_viewModeLabel;
@@ -45,6 +50,10 @@ public class HUD {
 	Hashtable<Upgrade, JButton> m_availableUpgrades;
 
 	public HUD(View view) {
+		ToolTipManager.sharedInstance().setInitialDelay(0);
+		UIManager.put("ToolTip.background", new Color(45, 105, 45));
+		LineBorder tooltipBorder = (LineBorder) BorderFactory.createLineBorder(Color.BLACK);
+		UIManager.put("ToolTip.border", tooltipBorder);
 
 		m_view = view;
 
@@ -76,7 +85,7 @@ public class HUD {
 				new ImageIcon("sprites/VueEnemyIcon.png").getImage().getScaledInstance(64, 64, Image.SCALE_DEFAULT));
 		m_weaponArray[5] = new ImageIcon(
 				new ImageIcon("sprites/MarkerSingle.png").getImage().getScaledInstance(64, 64, Image.SCALE_DEFAULT));
-		
+
 		ImageIcon mineralsIcone = new ImageIcon(
 				new ImageIcon("sprites/Minerals.png").getImage().getScaledInstance(64, 64, Image.SCALE_DEFAULT));
 		ImageIcon toolsIcone = new ImageIcon(
@@ -86,21 +95,21 @@ public class HUD {
 		JLabel toolsImage = new JLabel(toolsIcone);
 		toolsImage.setMaximumSize(new Dimension(100, 0));
 		m_weaponImage = new JLabel(m_weaponArray[0]);
-		
+
 		JPanel parentMineralsPanel = new JPanel();
 		JPanel parentToolsPanel = new JPanel();
 		parentMineralsPanel.setOpaque(false);
 		parentToolsPanel.setOpaque(false);
 		BoxLayout parentMineralsLayout = new BoxLayout(parentMineralsPanel, BoxLayout.Y_AXIS);
 		BoxLayout parentToolsLayout = new BoxLayout(parentToolsPanel, BoxLayout.Y_AXIS);
-		
+
 		m_mineralsLabel = new JLabel("0");
 		m_mineralsLabel.setForeground(Color.RED);
 		m_mineralsLabel.setFont(fontWest);
 		m_toolsLabel = new JLabel("0");
 		m_toolsLabel.setForeground(Color.RED);
 		m_toolsLabel.setFont(fontWest);
-		
+
 		m_mineralsLabel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 		m_toolsLabel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 		mineralsImage.setAlignmentX(JPanel.CENTER_ALIGNMENT);
@@ -111,15 +120,15 @@ public class HUD {
 		mineralsPanel.setBackground(Color.BLACK);
 		Border mineralsPanelBorder = BorderFactory.createLineBorder(Color.GRAY);
 		mineralsPanel.setBorder(mineralsPanelBorder);
-		mineralsPanel.setMaximumSize(new Dimension(90,0));
+		mineralsPanel.setMaximumSize(new Dimension(90, 0));
 		mineralsPanel.add(m_mineralsLabel);
 		JPanel toolsPanel = new JPanel();
 		toolsPanel.setBackground(Color.BLACK);
 		Border toolsPanelBorder = BorderFactory.createLineBorder(Color.GRAY);
 		toolsPanel.setBorder(toolsPanelBorder);
-		toolsPanel.setMaximumSize(new Dimension(90,0));
+		toolsPanel.setMaximumSize(new Dimension(90, 0));
 		toolsPanel.add(m_toolsLabel);
-		
+
 		Border mineralsBorder = BorderFactory.createLineBorder(Color.BLACK);
 		TitledBorder mineralsTitledBorder = BorderFactory.createTitledBorder(mineralsBorder, "Minerals");
 		mineralsTitledBorder.setTitleColor(Color.BLACK);
@@ -136,10 +145,10 @@ public class HUD {
 		m_weaponTitledBorder.setTitleJustification(TitledBorder.CENTER);
 		m_weaponImage.setMaximumSize(new Dimension(100, 0));
 		m_weaponImage.setBorder(m_weaponTitledBorder);
-		
+
 		parentMineralsPanel.setLayout(parentMineralsLayout);
 		parentToolsPanel.setLayout(parentToolsLayout);
-		
+
 		parentMineralsPanel.add(Box.createVerticalGlue());
 		parentMineralsPanel.add(mineralsImage);
 		parentMineralsPanel.add(Box.createVerticalGlue());
@@ -150,7 +159,7 @@ public class HUD {
 		parentToolsPanel.add(Box.createVerticalGlue());
 		parentToolsPanel.add(toolsPanel);
 		parentToolsPanel.add(Box.createVerticalGlue());
-		
+
 		MinToolsWeapon.add(Box.createVerticalGlue());
 		MinToolsWeapon.add(parentMineralsPanel);
 		MinToolsWeapon.add(Box.createVerticalGlue());
@@ -167,10 +176,10 @@ public class HUD {
 
 //		m_HPStamina avec les deux barres d'HP et DP
 //		m_HPStamina.setPreferredSize(new Dimension(100, 100));
-		
+
 		BasicProgressBarUI progressUIHealth = new BasicProgressBarUI();
 		BasicProgressBarUI progressUIDrone = new BasicProgressBarUI();
-		
+
 		HPStamina.add(Box.createVerticalGlue());
 		LineBorder progressBarBorder = (LineBorder) BorderFactory.createLineBorder(Color.BLACK, 3);
 		m_health = new JProgressBar(JProgressBar.VERTICAL, 0, 100);
@@ -195,8 +204,6 @@ public class HUD {
 		m_West.add(HPStamina);
 		m_West.add(MinToolsWeapon);
 
-		
-		
 		// ATH droit
 
 		// La font des futurs labels
@@ -256,7 +263,7 @@ public class HUD {
 		// Border de l'horloge
 		Border timePanelBorder = BorderFactory.createLineBorder(Color.GRAY);
 		timePanel.setBorder(timePanelBorder);
-		timePanel.setMaximumSize(new Dimension(90,20));
+		timePanel.setMaximumSize(new Dimension(90, 20));
 
 		// Label Temps
 		m_time = new JLabel("0 : 00");
@@ -265,9 +272,9 @@ public class HUD {
 		m_time.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 
 		timePanel.add(m_time);
-		m_East.add(new Box.Filler(new Dimension(0,0), new Dimension(0,5), new Dimension(0,10)));
+		m_East.add(new Box.Filler(new Dimension(0, 0), new Dimension(0, 5), new Dimension(0, 10)));
 		m_East.add(timePanel);
-		m_East.add(new Box.Filler(new Dimension(0,0), new Dimension(0,5), new Dimension(0,10)));
+		m_East.add(new Box.Filler(new Dimension(0, 0), new Dimension(0, 5), new Dimension(0, 10)));
 		m_East.add(Stats);
 
 		// Espace munition
@@ -300,97 +307,97 @@ public class HUD {
 		upgradeTitledBorder.setTitleColor(Color.BLACK);
 		upgradeTitledBorder.setTitleJustification(TitledBorder.CENTER);
 		m_upgrade.setBorder(upgradeTitledBorder);
-		
+
 		// Ajout d'une border au bouton
 		Border inset = BorderFactory.createEmptyBorder(10, 10, 10, 10);
 		LineBorder buttonLineBorder = (LineBorder) BorderFactory.createLineBorder(new Color(52, 109, 46));
 		Border buttonBorder = BorderFactory.createCompoundBorder(buttonLineBorder, inset);
 
-		//TODO implémenter un affichage dynamique pour les upgrade après avoir implémenté les upgrades
+		// TODO implémenter un affichage dynamique pour les upgrade après avoir
+		// implémenté les upgrades
 		LinkedList<Upgrade> statUpgrades = Model.getModel().getStatUpgrade();
 		LinkedList<JButton> buttons = new LinkedList<JButton>();
-		for(Upgrade upg : statUpgrades) {
+		for (Upgrade upg : statUpgrades) {
 			int eleCost = upg.getCostElec();
 			int minCost = upg.getCostMine();
-			upgradeButton upgradeButton = new upgradeButton("Upgrade",toolsIcone,mineralsIcone,eleCost,minCost);
+			upgradeButton upgradeButton = new upgradeButton("Tourelle De", "BONHJOUR",toolsIcone, mineralsIcone, eleCost, minCost);
 			upgradeButton.setBorder(buttonBorder);
+			upgradeButton.setEnabled(false);
 			buttons.add(upgradeButton);
 		}
 
-		
-		//Espace bouton
+		// Espace bouton
 		JPanel statUpgrade = new JPanel();
-		statUpgrade.setPreferredSize(new Dimension(110,500));
+		statUpgrade.setPreferredSize(new Dimension(110, 500));
 		statUpgrade.setBackground(Color.DARK_GRAY);
-		
+
 		for (JButton jButton : buttons) {
 			statUpgrade.add(jButton);
 		}
-		
-		//Le scrollPane
+
+		// Le scrollPane
 		JScrollPane scrollbutton = new JScrollPane(statUpgrade);
-		scrollbutton.setPreferredSize(new Dimension(110,100));
+		scrollbutton.setPreferredSize(new Dimension(110, 100));
 		scrollbutton.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollbutton.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollbutton.setBorder(null);
-		
-		//Une scrollbar
+
+		// Une scrollbar
 		JScrollBar scrollBar = new JScrollBar();
-		scrollBar.setPreferredSize(new Dimension(5,100));
+		scrollBar.setPreferredSize(new Dimension(5, 100));
 		scrollBar.setBackground(Color.GREEN);
 		scrollBar.setForeground(Color.RED);
-		
-		//UI Du scrollbar
+
+		// UI Du scrollbar
 		BasicScrollBarUI scrollUI = new BasicScrollBarUI() {
 			@Override
 			protected JButton createIncreaseButton(int orientation) {
 				JButton button = new JButton();
-				Dimension dim = new Dimension(0,0);
+				Dimension dim = new Dimension(0, 0);
 				button.setMaximumSize(dim);
 				button.setMinimumSize(dim);
 				button.setPreferredSize(dim);
 				return button;
 			}
-			
+
 			@Override
 			protected JButton createDecreaseButton(int orientation) {
 				JButton button = new JButton();
-				Dimension dim = new Dimension(0,0);
+				Dimension dim = new Dimension(0, 0);
 				button.setMaximumSize(dim);
 				button.setMinimumSize(dim);
 				button.setPreferredSize(dim);
 				return button;
 			}
-			
+
 			@Override
 			protected void configureScrollBarColors() {
 				super.configureScrollBarColors();
 				thumbColor = new Color(52, 109, 46);
 				trackColor = Color.BLACK;
 			}
-			
+
 			@Override
-	    protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds)
-	    {
-	        if(thumbBounds.isEmpty() || !scrollbar.isEnabled())     {
-	            return;
-	        }
+			protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
+				if (thumbBounds.isEmpty() || !scrollbar.isEnabled()) {
+					return;
+				}
 
-	        int w = thumbBounds.width;
-	        int h = thumbBounds.height;
+				int w = thumbBounds.width;
+				int h = thumbBounds.height;
 
-	        g.translate(thumbBounds.x, thumbBounds.y);
-	        g.setColor(thumbColor);
-	        g.fillRect(0, 0, w, h);
-	    }
+				g.translate(thumbBounds.x, thumbBounds.y);
+				g.setColor(thumbColor);
+				g.fillRect(0, 0, w, h);
+			}
 		};
 		scrollBar.setUI(scrollUI);
 		scrollBar.setUnitIncrement(30);
 		scrollbutton.setVerticalScrollBar(scrollBar);
-		
+
 		m_upgrade.add(scrollbutton);
 		m_East.add(m_upgrade);
-		
+
 		m_viewModePanel = new JPanel();
 		Border viewModeBorder = BorderFactory.createLineBorder(Color.BLACK);
 		TitledBorder viewModeTitledBorder = BorderFactory.createTitledBorder(viewModeBorder, "View Mode :");
@@ -400,7 +407,7 @@ public class HUD {
 		m_viewModePanel.setBackground(Color.DARK_GRAY);
 		m_viewModeLabel = new JLabel(m_weaponArray[3]);
 		m_viewModePanel.add(m_viewModeLabel);
-		
+
 	}
 
 //TODO
@@ -411,8 +418,8 @@ public class HUD {
 		TankBody tankBody = tank.getBody();
 		Turret tankTurret = tank.getTurret();
 		Entity played = model.getPlayed();
-		
-		//Timer
+
+		// Timer
 		long time = model.getTime();
 		long seconde = time / 1000;
 		long minutes = seconde / 60;
@@ -432,8 +439,8 @@ public class HUD {
 		} else {
 			timeString += seconde;
 		}
-		
-		//Boussole
+
+		// Boussole
 		m_time.setText(timeString);
 		float x = played.getX() + played.getWidth() / 2;
 		float y = played.getY() + played.getHeight() / 2;
@@ -460,17 +467,16 @@ public class HUD {
 
 		Model.getModel().getDrone();
 		m_level.setText("Level : ".concat(Integer.toString(tankBody.getLevel())));
-		
-		//Barres HP et Drone
+
+		// Barres HP et Drone
 		m_health.setMaximum(tankBody.getMaxHealth());
 		m_health.setValue(tankBody.getHealth());
 		m_drone.setMaximum(drone.getMaxHealth());
 		m_drone.setValue(drone.getHealth());
 
-		//Minerals, Electronics, Weapons et Markers
+		// Minerals, Electronics, Weapons et Markers
 		m_toolsLabel.setText(Integer.toString(tank.getInventory().getQuantity(MaterialType.ELECTRONIC)));
-		m_mineralsLabel
-				.setText(Integer.toString(tank.getInventory().getQuantity(MaterialType.MINERAL)));
+		m_mineralsLabel.setText(Integer.toString(tank.getInventory().getQuantity(MaterialType.MINERAL)));
 		switch (model.getVisionType()) {
 			case TANK:
 				m_weaponTitledBorder.setTitle("Weapon");
@@ -482,14 +488,12 @@ public class HUD {
 				m_East.remove(m_viewModePanel);
 				m_East.add(m_upgrade);
 				m_East.repaint();
-								
+
 				/*
-				 * for each m_availableUpgrade as upgrade
-				 * if upgrade.available() == true
+				 * for each m_availableUpgrade as upgrade if upgrade.available() == true
 				 * m_upgrade.add(m_availableUpgrade.get(upgrade));
 				 */
-				
-				
+
 				break;
 			case RESSOURCES:
 				m_weaponTitledBorder.setTitle("Marker");
@@ -498,7 +502,7 @@ public class HUD {
 				m_ammo.setText(markers.size() + " / " + drone.getMaxMarkers());
 				m_ammoPanel.setBorder(m_ammoTitledBorder);
 				m_ammoPanel.repaint();
-				
+
 				m_East.remove(m_upgrade);
 				m_viewModeLabel.setIcon(m_weaponArray[3]);
 				m_East.add(m_viewModePanel);
@@ -511,7 +515,7 @@ public class HUD {
 				m_ammo.setText(markers.size() + " / " + drone.getMaxMarkers());
 				m_ammoPanel.setBorder(m_ammoTitledBorder);
 				m_ammoPanel.repaint();
-				
+
 				m_East.remove(m_upgrade);
 				m_viewModeLabel.setIcon(m_weaponArray[4]);
 				m_East.add(m_viewModePanel);
@@ -548,48 +552,114 @@ public class HUD {
 			return THROUGH_NEGATIVE;
 		}
 	}
-	
+
 	private class upgradeButton extends JButton {
 
 		private boolean m_isLocked;
-		private int m_elecCost;
-		private int m_mineCost;
+		private String m_elecCost;
+		private String m_mineCost;
 		private Image m_electronics;
 		private Image m_minerals;
 		private String m_text;
-		
-		public upgradeButton(String string,ImageIcon elec,ImageIcon mine,int eCost,int mCost) {
+		private String m_descript;
+
+		public upgradeButton(String string, String descript, ImageIcon elec, ImageIcon mine, int eCost, int mCost) {
 			super();
-			m_elecCost = eCost;
-			m_mineCost = mCost;
+			m_elecCost = Integer.toString(eCost);
+			m_mineCost = Integer.toString(mCost);
 			m_text = string;
+			m_descript = descript;
 			m_electronics = elec.getImage();
 			m_minerals = mine.getImage();
 			this.setForeground(Color.BLACK);
 			this.setBackground(new Color(29, 73, 25));
-			this.setPreferredSize(new Dimension(95,50));
+			this.setPreferredSize(new Dimension(95, 60));
 			BasicButtonUI buttonUI = new BasicButtonUI() {
 				@Override
-				protected void paintButtonPressed(Graphics g, AbstractButton b){
-					System.out.println("YES");
+				protected void paintButtonPressed(Graphics g, AbstractButton b) {
 					Color color = new Color(0F, 0F, 0F, 0.3F);
 					g.setColor(color);
 					g.fillRect(0, 0, b.getWidth(), b.getHeight());
 				}
 			};
 			this.setUI(buttonUI);
+			this.setToolTipText(m_descript);
 		}
 
 		@Override
 		public void paint(Graphics g) {
 			super.paint(g);
-			g.drawRect(0, 0, this.getWidth()-1, this.getHeight()-1);
-			g.drawLine(0, 30, this.getWidth()-1, 30);
-			g.drawLine(this.getWidth()/2, 30, this.getWidth()/2, this.getHeight()-1);
-			g.drawImage(m_minerals, 3, 33, 15,15, null);
-			g.drawImage(m_electronics, 3+this.getWidth()/2, 33, 15,15, null);
+			int space = getHeight()-20;
+			g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
+			g.drawLine(0, space, getWidth() - 1, space);
+			g.drawLine(getWidth() / 2, space, getWidth() / 2, getHeight() - 1);
+			g.drawImage(m_minerals, 3, space + 3, 15, 15, null);
+			g.drawImage(m_electronics, 3 + getWidth() / 2, space+ 3, 15, 15, null);
+			FontRenderContext frc = new FontRenderContext(null, true, false);
+			
+//			Rectangle2D rect = g.getFont().getStringBounds(m_text, 0, m_text.length(), frc);
+//			g.drawString(m_text, (int) ((getWidth() - rect.getWidth()) / 2), (int) ((30 + rect.getHeight()) / 2));
+			Rectangle2D rectEle = g.getFont().getStringBounds(m_mineCost, 0, m_mineCost.length(), frc);
+			Rectangle2D rectMin = g.getFont().getStringBounds(m_elecCost, 0, m_elecCost.length(), frc);
+			g.drawString(m_mineCost, 6 + 15, getHeight() - 5);
+			g.drawString(m_elecCost, 6 + 15 + getWidth() / 2, getHeight() - 5);
+			paintTitle(g,space);
+			if (!isEnabled()) {
+				Color color = new Color(0F, 0F, 0F, 0.60F);
+				g.setColor(color);
+				g.fillRect(0, 0, this.getWidth(), this.getHeight());
+			}
 		}
-		
-	}
 
+		private void paintTitle(Graphics g,int space) {
+			FontRenderContext frc = new FontRenderContext(null, true, false);
+			String[] mots = m_text.split(" ");
+			Font font = g.getFont();
+			Rectangle2D mot1,mot2,mot3;
+			int x,y;
+			switch(mots.length) {
+				case 1:
+					font = font.deriveFont(15F);
+					mot1 = font.getStringBounds(mots[0], 0, mots[0].length(), frc);
+					g.setFont(font);
+					y = (int) (space - ((double)(space-mot1.getHeight())/2));
+					System.out.println("y");
+					x = (int) ((double)(getWidth()-mot1.getWidth())/2);
+					g.drawString(mots[0], x, y);
+					break;
+				case 2:
+					font = font.deriveFont(14F);
+					mot1 = font.getStringBounds(mots[0], 0, mots[0].length(), frc);
+					mot2 = font.getStringBounds(mots[1], 0, mots[1].length(), frc);
+					g.setFont(font);
+					double space2 = (double)space/2;
+					y = (int) (space2 - ((double)(space2 - mot1.getHeight())/2));
+					x = (int) ((double)(getWidth()-mot1.getWidth())/2);
+					g.drawString(mots[0], x, y-1);
+					y = (int) (space - ((space2 - mot2.getHeight())/2));
+					x = (int) ((double)(getWidth()-mot2.getWidth())/2);
+					g.drawString(mots[1], x, y-1);
+					break;
+				case 3:
+					font = font.deriveFont(12F);
+					mot1 = font.getStringBounds(mots[0], 0, mots[0].length(), frc);
+					mot2 = font.getStringBounds(mots[1], 0, mots[1].length(), frc);
+					mot3 = font.getStringBounds(mots[2], 0, mots[2].length(), frc);
+					g.setFont(font);
+					double space3 = (double)space/3;
+					y = (int) (space3 - ((double)(space3 - mot1.getHeight())/2));
+					x = (int) ((double)(getWidth()-mot1.getWidth())/2);
+					g.drawString(mots[0], x, y-1);
+					y = (int) (space3*2 - ((space3 - mot2.getHeight())/2));
+					x = (int) ((double)(getWidth()-mot2.getWidth())/2);
+					g.drawString(mots[1], x, y-1);
+					y = (int) (space - ((space3 - mot3.getHeight())/2));
+					x = (int) ((double)(getWidth()-mot3.getWidth())/2);
+					g.drawString(mots[2], x, y-1);
+					break;
+			}
+			
+			
+		}
+	}
 }

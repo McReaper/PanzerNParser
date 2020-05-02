5a62d54aeabb393cfdfbdc71e8a8bd51be901b8a
Victor MALOD <victor.malod@etu.univ-grenoble-alpes.fr>
2020-05-02 08:52:13 +0200

    merge ATH + modif isShown

--- workspace/groupe2.info3.game/panzernparser/info3/game/model/entities/Droppable.java
+++ workspace/groupe2.info3.game/panzernparser/info3/game/model/entities/Droppable.java
@@ -3,6 +3,8 @@ package info3.game.model.entities;
 import info3.game.automaton.Automaton;
 import info3.game.automaton.MyCategory;
 import info3.game.model.MaterialType;
+import info3.game.model.Model;
+import info3.game.model.Model.VisionType;
 
 public class Droppable extends StaticEntity {
 	public final static int DROPPABLE_WIDTH = 1;
@@ -35,6 +37,11 @@ public class Droppable extends StaticEntity {
 		m_category = MyCategory.P;
 	}
 
+	@Override
+	public boolean isShown() {
+		return (Model.getModel().getVisionType() != VisionType.ENEMIES);
+	}
+	
 	public MaterialType getMType() {
 		return m_mType;
 	}

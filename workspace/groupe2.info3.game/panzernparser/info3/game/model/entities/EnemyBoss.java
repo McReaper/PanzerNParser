bc6b20dd03498b977a1efbf3c1c5d5ff2e90f25e
emilie <emilie.tondeux@etu.univ-grenoble-alpes.fr>
2020-05-07 10:54:33 +0200

    creation des classes vides

---
+++ workspace/groupe2.info3.game/panzernparser/info3/game/model/entities/EnemyBoss.java
@@ -0,0 +1,18 @@
+package info3.game.model.entities;
+
+import info3.game.automaton.Automaton;
+
+public class EnemyBoss extends Enemy {
+
+	public EnemyBoss(int x, int y, int width, int height, Automaton aut) {
+		super(x, y, width, height, aut);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void levelUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ workspace/groupe2.info3.game/panzernparser/info3/game/model/entities/ShotEnemyBasic.java
@@ -0,0 +1,12 @@
+package info3.game.model.entities;
+
+import info3.game.automaton.Automaton;
+
+public class ShotEnemyBasic extends ShotEnemy{
+
+	public ShotEnemyBasic(int x, int y, int width, int height, Automaton aut) {
+		super(x, y, width, height, aut);
+		// TODO Auto-generated constructor stub
+	}
+
+}

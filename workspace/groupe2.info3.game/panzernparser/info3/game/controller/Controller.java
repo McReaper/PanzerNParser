826b59d0244dd9c3b8c905cfff7d7311f0ada008
Victor MALOD <victor.malod@etu.univ-grenoble-alpes.fr>
2020-05-08 11:41:18 +0200

    enleve un syso nul

--- workspace/groupe2.info3.game/panzernparser/info3/game/controller/Controller.java
+++ workspace/groupe2.info3.game/panzernparser/info3/game/controller/Controller.java
@@ -54,7 +54,6 @@ public class Controller implements GameCanvasListener {
 
 	public void loadMusic(String name) {
 		GameMain game = GameMain.getGame();
-		System.out.println(name);
 		File file = game.getSounds().get(name);
 		FileInputStream fis = null;
 		try {
